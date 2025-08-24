b1995915fcf4793dccc57bba6497d4340013ddeb
Matias Agnese <yosoymatiasn@hotmail.com>
2025-08-24 16:38:28 -0400

    final

---
+++ src/utils/testimonialImageImport.ts
@@ -0,0 +1,28 @@
+// testimonialImageMap.ts
+import ashley from "../assets/images/testimonials/ashley.jpg";
+import beth from "../assets/images/testimonials/beth.jpg";
+import betty from "../assets/images/testimonials/betty.jpg";
+import carlos from "../assets/images/testimonials/carlos.jpg";
+import chappell from "../assets/images/testimonials/chappell.jpg";
+import diane from "../assets/images/testimonials/diane.jpg";
+import jo from "../assets/images/testimonials/jo.jpg";
+import jonathan from "../assets/images/testimonials/jonathan.jpg";
+import karl from "../assets/images/testimonials/karl.jpg";
+import matias from "../assets/images/testimonials/matias.jpg";
+import michael from "../assets/images/testimonials/michael.jpg";
+import wendy from "../assets/images/testimonials/wendy.jpg";
+
+export const testimonialImages = {
+    ashley,
+    beth,
+    betty,
+    carlos,
+    chappell,
+    diane,
+    jo,
+    jonathan,
+    karl,
+    matias,
+    michael,
+    wendy,
+} 
