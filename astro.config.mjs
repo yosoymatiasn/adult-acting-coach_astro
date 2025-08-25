aa65610b69daf8d84a0757867021fea7f5f9a7cd
Matias Agnese <yosoymatiasn@hotmail.com>
2025-08-25 17:08:56 -0400

    fixed GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import { defineConfig } from 'astro/config'
 
 import tailwindcss from '@tailwindcss/vite'
 
-import partytown from '@astrojs/partytown'
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-
-  integrations: [partytown()],
 })
