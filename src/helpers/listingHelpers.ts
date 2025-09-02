47f93b7ceea717f84874ad710725d1e29c894e15
Matias Agnese <yosoymatiasn@hotmail.com>
2025-09-02 15:12:48 -0400

    updated class links

--- src/helpers/listingHelpers.ts
+++ src/helpers/listingHelpers.ts
@@ -1,6 +1,24 @@
-import { classesWithShowcases, type TClass, type TClassListing, type TShowListing } from '../data/classes';
+import {
+  classesWithShowcases,
+  type TClass,
+  type TClassListing,
+  type TShowListing,
+} from "../data/classes";
 
-
+export const getFormattedDate = (dateString: string): string[] => {
+  // Format date
+  const dateObj = new Date(dateString);
+  const formattedDate = dateObj.toLocaleDateString("en-US", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+  });
+  const formattedTime = dateObj.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  return [formattedDate, formattedTime];
+};
 
 /**
  * Flattens all upcoming class entries into a single array
@@ -21,18 +39,18 @@ export const flattenClasses = (classes: TClass[]): TClassListing[] => {
       type: classItem.type,
       duration: classItem.duration,
       upcoming,
-    }))
+    })),
   );
 
   // Filter out past dates
   const futureClasses = flattened.filter(
-    (item) => new Date(item.upcoming.date) > now
+    (item) => new Date(item.upcoming.date) > now,
   );
 
   // Sort by date ascending
   futureClasses.sort(
     (a, b) =>
-      new Date(a.upcoming.date).getTime() - new Date(b.upcoming.date).getTime()
+      new Date(a.upcoming.date).getTime() - new Date(b.upcoming.date).getTime(),
   );
 
   return futureClasses;
@@ -49,20 +67,20 @@ export const flattenShows = (classes: TClass[]): TShowListing[] => {
         title: classItem.show!.title,
         image: classItem.show!.image,
         description: classItem.show!.description,
-        upcoming: show
-      }))
+        upcoming: show,
+      })),
     );
 
   // Filter out past dates
   const futureShows = flattened.filter(
-    (item) => new Date(item.upcoming.date) > now
+    (item) => new Date(item.upcoming.date) > now,
   );
 
   // Sort by date ascending
   futureShows.sort(
     (a, b) =>
-      new Date(a.upcoming.date).getTime() - new Date(b.upcoming.date).getTime()
+      new Date(a.upcoming.date).getTime() - new Date(b.upcoming.date).getTime(),
   );
 
   return futureShows;
-}
+};
